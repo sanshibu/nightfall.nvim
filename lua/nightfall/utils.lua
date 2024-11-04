bc66e4d181240a00699a32d7c3fdfed13425100e
sanshibu <sanshibu2@gmail.com>
2024-11-04 14:51:21 +1100

    new changes

--- lua/nightfall/utils.lua
+++ lua/nightfall/utils.lua
@@ -7,71 +7,34 @@ function M.hex2rgb(hex)
 	return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
 end
 
--- Format value for highlight command
-local function format_value(val)
-	if type(val) == "string" and val:match("^#") then
-		-- Wrap hex colors in quotes
-		return string.format("'%s'", val)
-	elseif type(val) == "string" then
-		-- Check if the value contains spaces or special characters
-		if val:match("[%s%p]") then
-			return string.format("'%s'", val)
-		end
-	end
-	return val
-end
-
 -- Set highlight group
 function M.highlight(group, style)
-	local style_with_attrs = vim.tbl_extend("force", {}, style)
+	local hl = {}
+
+	-- Convert foreground color
+	if style.fg then
+		hl.fg = style.fg
+	end
+
+	-- Convert background color
+	if style.bg then
+		hl.bg = style.bg
+	end
+
+	-- Convert special color (for undercurl)
+	if style.sp then
+		hl.sp = style.sp
+	end
 
 	-- Convert style attributes
-	local attrs = {}
-	if style_with_attrs.bold then
-		attrs[#attrs + 1] = "bold"
-	end
-	if style_with_attrs.italic then
-		attrs[#attrs + 1] = "italic"
-	end
-	if style_with_attrs.underline then
-		attrs[#attrs + 1] = "underline"
-	end
-	if style_with_attrs.undercurl then
-		attrs[#attrs + 1] = "undercurl"
-	end
-	if style_with_attrs.strikethrough then
-		attrs[#attrs + 1] = "strikethrough"
-	end
+	hl.bold = style.bold
+	hl.italic = style.italic
+	hl.underline = style.underline
+	hl.undercurl = style.undercurl
+	hl.strikethrough = style.strikethrough
 
-	-- Remove boolean attributes
-	style_with_attrs.bold = nil
-	style_with_attrs.italic = nil
-	style_with_attrs.underline = nil
-	style_with_attrs.undercurl = nil
-	style_with_attrs.strikethrough = nil
-
-	-- Set highlight
-	style_with_attrs.sp = style_with_attrs.sp and style_with_attrs.sp or nil
-
-	-- Only add special if we have attributes
-	if #attrs > 0 then
-		style_with_attrs.special = table.concat(attrs, ",")
-	end
-
-	local hl_parts = {}
-	for key, val in pairs(style_with_attrs) do
-		if val and val ~= "NONE" then
-			local formatted_val = format_value(val)
-			table.insert(hl_parts, string.format("%s=%s", key, formatted_val))
-		end
-	end
-
-	if #hl_parts > 0 then
-		local hl_command = string.format("highlight %s %s", group, table.concat(hl_parts, " "))
-		vim.cmd(hl_command)
-	else
-		vim.cmd("highlight clear " .. group)
-	end
+	-- Set the highlight
+	vim.api.nvim_set_hl(0, group, hl)
 end
 
 return M
